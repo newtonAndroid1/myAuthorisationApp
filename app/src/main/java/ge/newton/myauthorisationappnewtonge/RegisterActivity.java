65d289306f565b187ee5db8278d237a3f2da1b9f
iona bartishvili <ibartishvili@tbcbank.com.ge>
2022-06-04 13:36:42 +0400

    version 3

--- app/src/main/java/ge/newton/myauthorisationappnewtonge/RegisterActivity.java
+++ app/src/main/java/ge/newton/myauthorisationappnewtonge/RegisterActivity.java
@@ -1,48 +1,49 @@
 package ge.newton.myauthorisationappnewtonge;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        // ჰერე
+        setContentView(R.layout.activity_register);
+
         EditText emailEditText = findViewById(R.id.emailAddressID);
         EditText passwordEditText = findViewById(R.id.passwordID);
+        EditText repeatPasswordEditText = findViewById(R.id.repeatPasswordID);
 
-        Button logInButton = findViewById(R.id.loginButton);
         Button registerButton = findViewById(R.id.registerButton);
 
-        logInButton.setOnClickListener(new View.OnClickListener() {
+        registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String email = emailEditText.getText().toString();
                 String password = passwordEditText.getText().toString();
+                String repeatPassword = repeatPasswordEditText.getText().toString();
                 if (email.isEmpty()) {
                     emailEditText.setError("Email is Empty");
                 }
                 if (!email.contains("@")) {
                     emailEditText.setError("invalid email");
                 }
-            }
-        });
 
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getBaseContext(), RegisterActivity.class));
+                if (password.isEmpty()) {
+                    passwordEditText.setError("password is Empty");
+                }
+                if (password.length() < 8) {
+                    passwordEditText.setError("pasword is invalid. password must contain at leas 8 characters");
+                }
+                if (password.equals(repeatPassword)) {
+                    passwordEditText.setError("pasword and repeatPassword is not the same");
+                    repeatPasswordEditText.setError("pasword and repeatPassword is not the same");
+                }
             }
-        });
+        } );
     }
 }
